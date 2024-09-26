ec2b4c53714cb5e8a956e604c4b285df51d6380d
Thieles Martins dos Reis <34041049+Thieles28@users.noreply.github.com>
2024-09-26 01:07:52 -0300

    Remover .idea e target do controle de versão

--- target/classes/db/migration/V2__create_conta.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS conta
-(
-    id              BIGSERIAL PRIMARY KEY,
-    data_vencimento DATE           NOT NULL,
-    data_pagamento  DATE,
-    valor           DECIMAL(10, 2) NOT NULL,
-    descricao       VARCHAR(255),
-    situacao        VARCHAR(50)    NOT NULL
-);
